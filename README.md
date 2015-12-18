a3c3e20fe35137a191394c7d7b0688ed1086d0e9
David K. Bainbridge <dbainbri@ciena.com>
2015-12-18 12:04:23 -0800

    closes #4, expose kafka producer configuration through ONOS config capability

--- README.md
+++ README.md
@@ -18,11 +18,34 @@ publish notifications for those devices and links for which it is the master.
 For links 'mastership' is determined by the destination device of the link.
 
 ## Configuration
-The Kafka instance to which the application binds defaults to `localhost:9092`.
-This can be configured via the ONOS command line using the `cfg get` and
-`cfg set` commands on the property
-`org.ciena.onos.KafkaNotificationBridge kafka-server` the value when setting
-the property should be `host:port`. 
+The Kafka configuraiton parameters have been exposed as ONOS options and can be
+queried and modified via the ONOS command line using the
+`cfg get org.ciena.onos.onos-kafka` and `cfg set org.ciena.onos.onos-kafka` 
+commands on the properties. The available options and there default values are:
+
+
+| Name | Default | Description |
+|---|---|---|
+|kafka.acks|1|The number of acknowledgments the producer requires the leader to have received before considering a request complete. This controls the durability of records that are sent|
+|kafka.batch.size|16384|The producer will attempt to batch records together into fewer requests whenever multiple records are being sent to the same partition. This helps performance on both the client and the server. This configuration controls the default batch size in bytes|
+|kafka.block.on.buffer.full|true|When our memory buffer is exhausted we must either stop accepting new records (block) or throw errors.|
+|kafka.bootstrap.servers|localhost:9092|A list of host/port pairs to use for establishing the initial connection to the Kafka cluster. The client will make use of all servers irrespective of which servers are specified here for bootstrapping—this list only impacts the initial hosts used to discover the full set of servers|
+|kafka.buffer.memory|33554432|The total bytes of memory the producer can use to buffer records waiting to be sent to the server|
+|kafka.client.id|ONOS Node ID|An id string to pass to the server when making requests.|
+|kafka.compression.type|none|Specify the final compression type for a given topic.|
+|kafka.linger.ms|1|The producer groups together any records that arrive in between request transmissions into a single batched request.|
+|kafka.max.in.flight.requests.per.connection|5|The maximum number of unacknowledged requests the client will send on a single connection before blocking.|
+|kafka.max.request.size|1048576|The maximum size of a request. This is also effectively a cap on the maximum record size.|
+|kafka.metadata.fetch.timeout|60000|The first time data is sent to a topic we must fetch metadata about that topic to know which servers host the topic's partitions. This fetch to succeed before throwing an exception back to the client.|
+|kafka.metadata.max.age|300000|he period of time in milliseconds after which we force a refresh of metadata even if we haven't seen any partition leadership changes to proactively discover any new brokers or partitions.|
+|kafka.metrics.num.samples|2|The number of samples maintained to compute metrics.|
+|kafka.metrics.sample.window.ms|30000|The number of samples maintained to compute metrics.|
+|kafka.receive.buffer|32768|The size of the TCP receive buffer (SO_RCVBUF) to use when reading data.|
+|kafka.reconnect.backoff.ms|10|The amount of time to wait before attempting to reconnect to a given host.|
+|kafka.retries|0|Setting a value greater than zero will cause the client to resend any record whose send fails with a potentially transient error.|
+|kafka.retry.backoff.ms|100|The amount of time to wait before attempting to retry a failed fetch request to a given topic partition.|
+|kafka.send.buffer|131072|The size of the TCP send buffer (SO_SNDBUF) to use when sending data.|
+|kafka.timeout|30000|The configuration controls the maximum amount of time the client will wait for the response of a request.|
 
 ## JSON Encoding
 The Kafka bridge application encodes the `DeviceEvent` and the `LinkEvent` as
--- src/main/java/org/ciena/onos/KafkaNotificationBridge.java
+++ src/main/java/org/ciena/onos/KafkaNotificationBridge.java
@@ -61,8 +61,55 @@ public class KafkaNotificationBridge {
 
 	private final static String DEVICE_TOPIC = "onos.device";
 	private final static String LINK_TOPIC = "onos.link";
-	private final static String KAFKA_SERVER_CONFIG = "kafka-server";
-	private final static String DEFAULT_KAFKA_SERVER = "localhost:9092";
+
+	/*
+	 * Property names used with ONOS to define properties that are used when
+	 * instantiating a kafka producer
+	 */
+	private final static String KAFKA_ACKS_CONFIG = "kafka.acks";
+	private final static String KAFKA_BATCH_SIZE_CONFIG = "kafka.batch.size";
+	private final static String KAFKA_BLOCK_ON_BUFFER_FULL_CONFIG = "kafka.block.on.buffer.full";
+	private final static String KAFKA_BOOTSTRAP_SERVERS_CONFIG = "kafka.bootstrap.servers";
+	private final static String KAFKA_BUFFER_MEMORY_CONFIG = "kafka.buffer.memory";
+	private final static String KAFKA_CLIENT_ID_CONFIG = "kafka.client.id";
+	private final static String KAFKA_COMPRESSION_TYPE_CONFIG = "kafka.compression.type";
+	private final static String KAFKA_LINGER_MS_CONFIG = "kafka.linger.ms";
+	private final static String KAFKA_MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION_CONFIG = "kafka.max.in.flight.requests.per.connection";
+	private final static String KAFKA_MAX_REQUEST_SIZE_CONFIG = "kafka.max.request.size";
+	private final static String KAFKA_METADATA_FETCH_TIMEOUT_CONFIG = "kafka.metadata.fetch.timeout";
+	private final static String KAFKA_METADATA_MAX_AGE_CONFIG = "kafka.metadata.max.age";
+	private final static String KAFKA_METRICS_NUM_SAMPLES_CONFIG = "kafka.metrics.num.samples";
+	private final static String KAFKA_METRICS_SAMPLE_WINDOW_MS_CONFIG = "kafka.metrics.sample.window.ms";
+	private final static String KAFKA_RECEIVE_BUFFER_CONFIG = "kafka.receive.buffer";
+	private final static String KAFKA_RECONNECT_BACKOFF_MS_CONFIG = "kafka.reconnect.backoff.ms";
+	private final static String KAFKA_RETRIES_CONFIG = "kafka.retries";
+	private final static String KAFKA_RETRY_BACKOFF_MS_CONFIG = "kafka.retry.backoff.ms";
+	private final static String KAFKA_SEND_BUFFER_CONFIG = "kafka.send.buffer";
+	private final static String KAFKA_TIMEOUT_CONFIG = "kafka.timeout";
+
+	/*
+	 * Default values used for instantiating a kafka producer
+	 */
+	private final static String DEFAULT_KAFKA_ACKS = "1";
+	private final static String DEFAULT_KAFKA_BATCH_SIZE = "16384";
+	private final static String DEFAULT_KAFKA_BLOCK_ON_BUFFER_FULL = "true";
+	private final static String DEFAULT_KAFKA_BOOTSTRAP_SERVERS = "localhost:9092";
+	private final static String DEFAULT_KAFKA_BUFFER_MEMORY = "33554432";
+	private final static String DEFAULT_KAFKA_CLIENT_ID = "";
+	private final static String DEFAULT_KAFKA_COMPRESSION_TYPE = "none";
+	private final static String DEFAULT_KAFKA_LINGER_MS = "1";
+	private final static String DEFAULT_KAFKA_MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION = "5";
+	private final static String DEFAULT_KAFKA_MAX_REQUEST_SIZE = "1048576";
+	private final static String DEFAULT_KAFKA_METADATA_FETCH_TIMEOUT = "60000";
+	private final static String DEFAULT_KAFKA_METADATA_MAX_AGE = "300000";
+	private final static String DEFAULT_KAFKA_METRICS_NUM_SAMPLES = "2";
+	private final static String DEFAULT_KAFKA_METRICS_SAMPLE_WINDOW_MS = "30000";
+	private final static String DEFAULT_KAFKA_RECEIVE_BUFFER = "32768";
+	private final static String DEFAULT_KAFKA_RECONNECT_BACKOFF_MS = "10";
+	private final static String DEFAULT_KAFKA_RETRIES = "0";
+	private final static String DEFAULT_KAFKA_RETRY_BACKOFF_MS = "100";
+	private final static String DEFAULT_KAFKA_SEND_BUFFER = "131072";
+	private final static String DEFAULT_KAFKA_TIMEOUT = "30000";
 
 	private DeviceListener deviceListener = null;
 	private LinkListener linkListener = null;
@@ -84,8 +131,65 @@ public class KafkaNotificationBridge {
 	@Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
 	protected ComponentConfigService configService;
 
-	@Property(name = KAFKA_SERVER_CONFIG, value = DEFAULT_KAFKA_SERVER, label = "Kafka server for initial connection")
-	private String kafkaServer = DEFAULT_KAFKA_SERVER;
+	@Property(name = "kafka.acks", value = DEFAULT_KAFKA_ACKS, label = "The number of acknowledgments the producer requires the leader to have received before considering a request complete. This controls the durability of records that are sent")
+	private String cfgKafkaAcks = DEFAULT_KAFKA_ACKS;
+
+	@Property(name = "kafka.batch.size", value = DEFAULT_KAFKA_BATCH_SIZE, label = "The producer will attempt to batch records together into fewer requests whenever multiple records are being sent to the same partition. This helps performance on both the client and the server. This configuration controls the default batch size in bytes.")
+	private String cfgKafkaBatchSize = DEFAULT_KAFKA_BATCH_SIZE;
+
+	@Property(name = "kafka.block.on.buffer.full", value = DEFAULT_KAFKA_BLOCK_ON_BUFFER_FULL, label = "When our memory buffer is exhausted we must either stop accepting new records (block) or throw errors.")
+	private String cfgKafkaBlockOnBufferFull = DEFAULT_KAFKA_BLOCK_ON_BUFFER_FULL;
+
+	@Property(name = "kafka.bootstrap.servers", value = DEFAULT_KAFKA_BOOTSTRAP_SERVERS, label = "A list of host/port pairs to use for establishing the initial connection to the Kafka cluster. The client will make use of all servers irrespective of which servers are specified here for bootstrapping—this list only impacts the initial hosts used to discover the full set of servers")
+	private String cfgKafkaBootstrapServers = DEFAULT_KAFKA_BOOTSTRAP_SERVERS;
+
+	@Property(name = "kafka.buffer.memory", value = DEFAULT_KAFKA_BUFFER_MEMORY, label = "The total bytes of memory the producer can use to buffer records waiting to be sent to the server")
+	private String cfgKafkaBufferMemory = DEFAULT_KAFKA_BUFFER_MEMORY;
+
+	@Property(name = "kafka.client.id", value = DEFAULT_KAFKA_CLIENT_ID, label = "An id string to pass to the server when making requests.")
+	private String cfgKafkaClientId = DEFAULT_KAFKA_CLIENT_ID;
+
+	@Property(name = "kafka.compression.type", value = DEFAULT_KAFKA_COMPRESSION_TYPE, label = "Specify the final compression type for a given topic.")
+	private String cfgKafkaCompressionType = DEFAULT_KAFKA_COMPRESSION_TYPE;
+
+	@Property(name = "kafka.linger.ms", value = DEFAULT_KAFKA_LINGER_MS, label = "The producer groups together any records that arrive in between request transmissions into a single batched request.")
+	private String cfgKafkaLingerMs = DEFAULT_KAFKA_LINGER_MS;
+
+	@Property(name = "kafka.max.in.flight.requests.per.connection", value = DEFAULT_KAFKA_MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, label = "The maximum number of unacknowledged requests the client will send on a single connection before blocking. ")
+	private String cfgKafkaMaxInFlightRequestsPerConnection = DEFAULT_KAFKA_MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION;
+
+	@Property(name = "kafka.max.request.size", value = DEFAULT_KAFKA_MAX_REQUEST_SIZE, label = "The maximum size of a request. This is also effectively a cap on the maximum record size.")
+	private String cfgKafkaMaxRequestSize = DEFAULT_KAFKA_MAX_REQUEST_SIZE;
+
+	@Property(name = "kafka.metadata.fetch.timeout", value = DEFAULT_KAFKA_METADATA_FETCH_TIMEOUT, label = "The first time data is sent to a topic we must fetch metadata about that topic to know which servers host the topic's partitions. This fetch to succeed before throwing an exception back to the client.")
+	private String cfgKafkaMetadataFetchTimeout = DEFAULT_KAFKA_METADATA_FETCH_TIMEOUT;
+
+	@Property(name = "kafka.metadata.max.age", value = DEFAULT_KAFKA_METADATA_MAX_AGE, label = "he period of time in milliseconds after which we force a refresh of metadata even if we haven't seen any partition leadership changes to proactively discover any new brokers or partitions.")
+	private String cfgKafkaMetadataMaxAge = DEFAULT_KAFKA_METADATA_MAX_AGE;
+
+	@Property(name = "kafka.metrics.num.samples", value = DEFAULT_KAFKA_METRICS_NUM_SAMPLES, label = "The number of samples maintained to compute metrics.")
+	private String cfgKafkaMetricsNumSamples = DEFAULT_KAFKA_METRICS_NUM_SAMPLES;
+
+	@Property(name = "kafka.metrics.sample.window.ms", value = DEFAULT_KAFKA_METRICS_SAMPLE_WINDOW_MS, label = "The number of samples maintained to compute metrics.")
+	private String cfgKafkaMetricsSampleWindowMs = DEFAULT_KAFKA_METRICS_SAMPLE_WINDOW_MS;
+
+	@Property(name = "kafka.receive.buffer", value = DEFAULT_KAFKA_RECEIVE_BUFFER, label = "The size of the TCP receive buffer (SO_RCVBUF) to use when reading data.")
+	private String cfgKafkaReceiveBuffer = DEFAULT_KAFKA_RECEIVE_BUFFER;
+
+	@Property(name = "kafka.reconnect.backoff.ms", value = DEFAULT_KAFKA_RECONNECT_BACKOFF_MS, label = "The amount of time to wait before attempting to reconnect to a given host.")
+	private String cfgKafkaReconnectBackoffMs = DEFAULT_KAFKA_RECONNECT_BACKOFF_MS;
+
+	@Property(name = "kafka.retries", value = DEFAULT_KAFKA_RETRIES, label = "Setting a value greater than zero will cause the client to resend any record whose send fails with a potentially transient error.")
+	private String cfgKafkaRetries = DEFAULT_KAFKA_RETRIES;
+
+	@Property(name = "kafka.retry.backoff.ms", value = DEFAULT_KAFKA_RETRY_BACKOFF_MS, label = "The amount of time to wait before attempting to retry a failed fetch request to a given topic partition.")
+	private String cfgKafkaRetryBackoffMs = DEFAULT_KAFKA_RETRY_BACKOFF_MS;
+
+	@Property(name = "kafka.send.buffer", value = DEFAULT_KAFKA_SEND_BUFFER, label = "The size of the TCP send buffer (SO_SNDBUF) to use when sending data.")
+	private String cfgKafkaSendBuffer = DEFAULT_KAFKA_SEND_BUFFER;
+
+	@Property(name = "kafka.timeout", value = DEFAULT_KAFKA_TIMEOUT, label = "The configuration controls the maximum amount of time the client will wait for the response of a request.")
+	private String cfgKafkaTimeout = DEFAULT_KAFKA_TIMEOUT;
 
 	private KafkaProducer<String, String> producer = null;
 
@@ -158,11 +262,138 @@ public class KafkaNotificationBridge {
 	public void modified(ComponentContext context) {
 		Dictionary<?, ?> properties = context.getProperties();
 
-		String value = Tools.get(properties, KAFKA_SERVER_CONFIG);
-		String newKafkaServer = Strings.isNullOrEmpty(value) ? DEFAULT_KAFKA_SERVER : value.trim();
-		if (!newKafkaServer.equals(kafkaServer)) {
-			log.info("Kafka server updated from '{}' to '{}'", kafkaServer, newKafkaServer);
-			createKafkaProducer(newKafkaServer);
+		/*
+		 * Something has changed in the configuration, so grab the new Kafka
+		 * options from the properties and if any have changed then close and
+		 * re-create a new Kafka producer.
+		 */
+		boolean isModified = false;
+		String value = Strings.nullToEmpty(Tools.get(properties, KAFKA_ACKS_CONFIG)).trim();
+		if (!value.isEmpty() && !value.equals(cfgKafkaAcks)) {
+			cfgKafkaAcks = value;
+			isModified = true;
+		}
+
+		value = Strings.nullToEmpty(Tools.get(properties, KAFKA_BATCH_SIZE_CONFIG)).trim();
+		if (!value.isEmpty() && !value.equals(cfgKafkaBatchSize)) {
+			cfgKafkaBatchSize = value;
+			isModified = true;
+		}
+
+		value = Strings.nullToEmpty(Tools.get(properties, KAFKA_BLOCK_ON_BUFFER_FULL_CONFIG)).trim();
+		if (!value.isEmpty() && !value.equals(cfgKafkaBlockOnBufferFull)) {
+			cfgKafkaBlockOnBufferFull = value;
+			isModified = true;
+		}
+
+		value = Strings.nullToEmpty(Tools.get(properties, KAFKA_BOOTSTRAP_SERVERS_CONFIG)).trim();
+		if (!value.isEmpty() && !value.equals(cfgKafkaBootstrapServers)) {
+			cfgKafkaBootstrapServers = value;
+			isModified = true;
+		}
+
+		value = Strings.nullToEmpty(Tools.get(properties, KAFKA_BUFFER_MEMORY_CONFIG)).trim();
+		if (!value.isEmpty() && !value.equals(cfgKafkaBufferMemory)) {
+			cfgKafkaBufferMemory = value;
+			isModified = true;
+		}
+
+		value = Strings.nullToEmpty(Tools.get(properties, KAFKA_CLIENT_ID_CONFIG)).trim();
+		if (!value.isEmpty() && !value.equals(cfgKafkaClientId)) {
+			cfgKafkaClientId = value;
+			isModified = true;
+		}
+
+		value = Strings.nullToEmpty(Tools.get(properties, KAFKA_COMPRESSION_TYPE_CONFIG)).trim();
+		if (!value.isEmpty() && !value.equals(cfgKafkaCompressionType)) {
+			cfgKafkaCompressionType = value;
+			isModified = true;
+		}
+
+		value = Strings.nullToEmpty(Tools.get(properties, KAFKA_LINGER_MS_CONFIG)).trim();
+		if (!value.isEmpty() && !value.equals(cfgKafkaLingerMs)) {
+			cfgKafkaLingerMs = value;
+			isModified = true;
+		}
+
+		value = Strings.nullToEmpty(Tools.get(properties, KAFKA_MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION_CONFIG)).trim();
+		if (!value.isEmpty() && !value.equals(cfgKafkaMaxInFlightRequestsPerConnection)) {
+			cfgKafkaMaxInFlightRequestsPerConnection = value;
+			isModified = true;
+		}
+
+		value = Strings.nullToEmpty(Tools.get(properties, KAFKA_MAX_REQUEST_SIZE_CONFIG)).trim();
+		if (!value.isEmpty() && !value.equals(cfgKafkaMaxRequestSize)) {
+			cfgKafkaMaxRequestSize = value;
+			isModified = true;
+		}
+
+		value = Strings.nullToEmpty(Tools.get(properties, KAFKA_METADATA_FETCH_TIMEOUT_CONFIG)).trim();
+		if (!value.isEmpty() && !value.equals(cfgKafkaMetadataFetchTimeout)) {
+			cfgKafkaMetadataFetchTimeout = value;
+			isModified = true;
+		}
+
+		value = Strings.nullToEmpty(Tools.get(properties, KAFKA_METADATA_MAX_AGE_CONFIG)).trim();
+		if (!value.isEmpty() && !value.equals(cfgKafkaMetadataMaxAge)) {
+			cfgKafkaMetadataMaxAge = value;
+			isModified = true;
+		}
+
+		value = Strings.nullToEmpty(Tools.get(properties, KAFKA_METRICS_NUM_SAMPLES_CONFIG)).trim();
+		if (!value.isEmpty() && !value.equals(cfgKafkaMetricsNumSamples)) {
+			cfgKafkaMetricsNumSamples = value;
+			isModified = true;
+		}
+
+		value = Strings.nullToEmpty(Tools.get(properties, KAFKA_METRICS_SAMPLE_WINDOW_MS_CONFIG)).trim();
+		if (!value.isEmpty() && !value.equals(cfgKafkaMetricsSampleWindowMs)) {
+			cfgKafkaMetricsSampleWindowMs = value;
+			isModified = true;
+		}
+
+		value = Strings.nullToEmpty(Tools.get(properties, KAFKA_RECEIVE_BUFFER_CONFIG)).trim();
+		if (!value.isEmpty() && !value.equals(cfgKafkaReceiveBuffer)) {
+			cfgKafkaReceiveBuffer = value;
+			isModified = true;
+		}
+
+		value = Strings.nullToEmpty(Tools.get(properties, KAFKA_RECONNECT_BACKOFF_MS_CONFIG)).trim();
+		if (!value.isEmpty() && !value.equals(cfgKafkaReconnectBackoffMs)) {
+			cfgKafkaReconnectBackoffMs = value;
+			isModified = true;
+		}
+
+		value = Strings.nullToEmpty(Tools.get(properties, KAFKA_RETRIES_CONFIG)).trim();
+		if (!value.isEmpty() && !value.equals(cfgKafkaRetries)) {
+			cfgKafkaRetries = value;
+			isModified = true;
+		}
+
+		value = Strings.nullToEmpty(Tools.get(properties, KAFKA_RETRY_BACKOFF_MS_CONFIG)).trim();
+		if (!value.isEmpty() && !value.equals(cfgKafkaRetryBackoffMs)) {
+			cfgKafkaRetryBackoffMs = value;
+			isModified = true;
+		}
+
+		value = Strings.nullToEmpty(Tools.get(properties, KAFKA_SEND_BUFFER_CONFIG)).trim();
+		if (!value.isEmpty() && !value.equals(cfgKafkaSendBuffer)) {
+			cfgKafkaSendBuffer = value;
+			isModified = true;
+		}
+
+		value = Strings.nullToEmpty(Tools.get(properties, KAFKA_TIMEOUT_CONFIG)).trim();
+		if (!value.isEmpty() && !value.equals(cfgKafkaTimeout)) {
+			cfgKafkaTimeout = value;
+			isModified = true;
+		}
+
+		if (isModified) {
+			log.info("Kafka options have been modified, reconnecting with modified values");
+			createKafkaProducer();
+		} else {
+			log.info(
+					"Configuration options have been modified, but values have not been changed, so action being performed based on modifications");
 		}
 	}
 
@@ -174,7 +405,7 @@ public class KafkaNotificationBridge {
 	 * @param newKafkaServer
 	 *            the server and port to which the producer should be connected
 	 */
-	private synchronized void createKafkaProducer(String newKafkaServer) {
+	private synchronized void createKafkaProducer() {
 
 		// Close and existing producer
 		if (producer != null) {
@@ -182,34 +413,54 @@ public class KafkaNotificationBridge {
 			producer = null;
 		}
 
-		// If they didn't specify the new Kafka server to which to connect,
-		// then this is either a re-connect or an initial connection
-		if (newKafkaServer != null) {
-			kafkaServer = newKafkaServer;
-		}
-
-		log.debug("Attempting to connect to Kafka at {} as client {}", kafkaServer,
+		log.debug("Attempting to connect to Kafka at {} as client {}", cfgKafkaBootstrapServers,
 				clusterService.getLocalNode().id().toString());
 
-		// TODO: These and others should be driven by configuration
 		Properties props = new Properties();
-		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
-		props.put(ProducerConfig.CLIENT_ID_CONFIG, clusterService.getLocalNode().id().toString());
-		props.put(ProducerConfig.ACKS_CONFIG, "1");
-		props.put(ProducerConfig.RETRIES_CONFIG, 1);
-		props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
-		props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
-		props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+		/*
+		 * Set the client ID to the local node ID if a value has not been set by
+		 * the configuration
+		 */
+		props.put(ProducerConfig.CLIENT_ID_CONFIG, (Strings.isNullOrEmpty(cfgKafkaClientId)
+				? clusterService.getLocalNode().id().toString() : cfgKafkaClientId));
+
+		/*
+		 * Set the serializer implementations, which is not something that can
+		 * be customized by the client.
+		 */
 		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+		/*
+		 * Set the properites that can be overriden by configuration
+		 */
+		props.put(ProducerConfig.ACKS_CONFIG, cfgKafkaAcks);
+		props.put(ProducerConfig.BATCH_SIZE_CONFIG, cfgKafkaBatchSize);
+		props.put(ProducerConfig.BLOCK_ON_BUFFER_FULL_CONFIG, cfgKafkaBlockOnBufferFull);
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, cfgKafkaBootstrapServers);
+		props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, cfgKafkaBufferMemory);
+		props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, cfgKafkaCompressionType);
+		props.put(ProducerConfig.LINGER_MS_CONFIG, cfgKafkaLingerMs);
+		props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, cfgKafkaMaxInFlightRequestsPerConnection);
+		props.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, cfgKafkaMaxRequestSize);
+		props.put(ProducerConfig.METADATA_FETCH_TIMEOUT_CONFIG, cfgKafkaMetadataFetchTimeout);
+		props.put(ProducerConfig.METADATA_MAX_AGE_CONFIG, cfgKafkaMetadataMaxAge);
+		props.put(ProducerConfig.METRICS_NUM_SAMPLES_CONFIG, cfgKafkaMetricsNumSamples);
+		props.put(ProducerConfig.METRICS_SAMPLE_WINDOW_MS_CONFIG, cfgKafkaMetricsSampleWindowMs);
+		props.put(ProducerConfig.RECEIVE_BUFFER_CONFIG, cfgKafkaReceiveBuffer);
+		props.put(ProducerConfig.RECONNECT_BACKOFF_MS_CONFIG, cfgKafkaReconnectBackoffMs);
+		props.put(ProducerConfig.RETRIES_CONFIG, cfgKafkaRetries);
+		props.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, cfgKafkaRetryBackoffMs);
+		props.put(ProducerConfig.SEND_BUFFER_CONFIG, cfgKafkaSendBuffer);
+		props.put(ProducerConfig.TIMEOUT_CONFIG, cfgKafkaTimeout);
+
 		try {
 			producer = new KafkaProducer<String, String>(props);
-			log.info("Connected to Kafka at {} as client {}", kafkaServer,
+			log.info("Connected to Kafka at {} as client {}", cfgKafkaBootstrapServers,
 					clusterService.getLocalNode().id().toString());
 		} catch (Exception e) {
 			log.error("Unable to create Kafka producer to {}, no events will be published on the Kafka bridge : {}",
-					kafkaServer, e.getClass().getName(), e.getMessage(), e);
+					cfgKafkaBootstrapServers, e.getClass().getName(), e.getMessage(), e);
 		}
 	}
 
@@ -226,7 +477,7 @@ public class KafkaNotificationBridge {
 			configService.registerProperties(this.getClass());
 		}
 
-		createKafkaProducer(null);
+		createKafkaProducer();
 
 		closure = new Callback() {
 			@Override
